4c4ed6395a3b11febcf16c8761c5cf7d248737d6
CParisel <cparisel@gmail.com>
2017-02-16 00:43:35 +0100

    BANO et SIREN ajout champs *normalises*

--- code/dbmain.py
+++ code/dbmain.py
@@ -1,6 +1,8 @@
 import json
 import sys
+import string
 from os import path
+from unidecode import unidecode
 
 from pandas import read_csv
 
@@ -29,23 +31,37 @@ def init(argv):
     data_path = cfg['datapath']
 
 
+
+'''
+BANO :
+- add columns ADD_num_voie and ADD_ind_rep to match SIREN NUMVOIE and INDREP
+- add columns CAPS_voie
+'''
 def prepare_data_bano(verbose=VERBOSE):
     data_file = path.join(data_path, cfg['BANO'])
     if verbose: print("Reading %s" % data_file)
     df = read_csv(data_file, sep=',', header=None, encoding='latin-1')
     df.columns = cfg['BANOHeader']  # all columns are kept
-    # create additional column with road type and road name
-    # df['voie_type'] = ''
-    # df['voie_denom'] = ''
-    # TODO
-
+    # create additional columns from numero
+    df['ADD_num_voie']= df['numero'].map(lambda x: x.rstrip("TERBIS"))
+    df['ADD_ind_rep']= df['numero'].map(lambda x: x.lstrip('0123456789'))
+    df['ADD_ind_rep']= df['ADD_ind_rep'].map(lambda x: x.replace('BIS','B'))
+    df['ADD_ind_rep'] = df['ADD_ind_rep'].map(lambda x: x.replace('TER', 'T'))
+    # additional column from voie : remove accent and put in caps
+    df['CAPS_voie']= df['voie'].map(lambda x: unidecode(x.upper()))
     if verbose:
         print("Kept %d lines" % df.shape[0])
         print("Quick look\n %s" % df.head(5))
 
     return df
 
-
+'''
+SIREN
+- drop useless columns
+- rename columns
+- filter by department
+- create full street name (
+'''
 def prepare_data_siren(verbose=VERBOSE):
     data_file = path.join(data_path, cfg['SIREN'])
     if verbose:
@@ -67,9 +83,22 @@ def prepare_data_siren(verbose=VERBOSE):
         df = df.loc[df[a] == filter_by[a]]
     if verbose:
         print("Rows after filtering by department : %d" % df.shape[0])
+    # create columns for address
+    '''
+    di= {'ALL':'ALLEE ', 'AV':'AVENUE ', 'BD':'BOULEVARD ', 'CHE';'CHEMIN ', 'CHS':'CHAUSSEE ', 'FG':'FAUBOURG ', 'IMP':'IMPASSE ', 'LOT':'LOTISSEMENT ', 'PL':'PLACE ', 'QUA':'QUARTIER ', 'QUAI':'QUAI ', 'RUE': 'RUE ', 'RTE':'ROUTE '}
+    df['ADD_TYPVOIE']= df['TYPVOIE'].replace(di)  # option : inplace=True
+    df['ADD_LIBVOIE']= df['LIBVOIE'].map(lambda x: unidecode(x))
+    # get ready for merging
+    df['ADD_VOIE']= df['ADD_TYPVOIE']+ df['ADD_LIBVOIE']
+    #df[df.ADD_TYPVOIE.isin(df.ADD_LIBVOIE)==False]['ADD_VOIE']=  df['ADD_TYPVOIE']+ df['ADD_LIBVOIE']
+
+
+
+
+
     # bano id
     df['BANO_ID'] = -1
-
+    '''
     if verbose:
         print("Kept %d rows" % df.shape[0])
         print("Quick look\n %s" % df.head(5))
@@ -81,7 +110,6 @@ Populate a new column in SIREN df with the id of the corresponding BANO line
 For each line of SIREN
     filter corresponding BANO with BANO.code_post == SIREN.CODPOS
         filter BANO.numero== SIREN.NUM_VOIE + f(SIREN.IND_REP)
-
 '''
 def join_data():
     if VERBOSE:
@@ -112,9 +140,12 @@ def match_data_with_same_code_post(sub_siren, sub_bano):
     return 0
 
 
+def test():
+    a= ['103BIS', '2', '24', '12TER']
+
 
 if __name__ == "__main__":
     init(sys.argv[1:])
-    df_bano = prepare_data_bano(verbose=False)
+    df_bano = prepare_data_bano(verbose=True)
     df_siren = prepare_data_siren(verbose=True)
     #join_data()
